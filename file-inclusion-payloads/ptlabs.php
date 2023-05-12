6b3e2756678032dac7b5fa8f644356ef074ce156
Paulo Mota <44026675+paulogmota@users.noreply.github.com>
2023-05-12 11:53:23 -0300

    Update ptlabs.php

--- file-inclusion-payloads/ptlabs.php
+++ file-inclusion-payloads/ptlabs.php
@@ -1,6 +1,6 @@
 <?php
     // Define the command to execute
-    $command = '/usr/local/bin/score daee4bbb-30f5-4c24-abf7-f5eeddc7b891';
+    $command = 'INSERT COMMAND, PATH, SCRIPT HERE';
 
     // Execute the command and get the output
     $output = exec($command);
